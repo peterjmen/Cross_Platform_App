d4efb9559b4bd0a00c424fcdf9942f53122be339
Paulo Gehlen <paulovitor_1@hotmail.com>
2023-06-20 18:37:49 +1200

    PAULO_PRACTICE

--- src/pages/MyExercisesPage.js
+++ src/pages/MyExercisesPage.js
@@ -7,7 +7,24 @@ function MyExercisesPage() {
     <div>
       <h1 className="page-title">My Exercises</h1>
       <SearchBar />
-      <p>This is the exercises page content.</p>
+      <div className="image-options">
+        <div className="image-option">
+          <label htmlFor="image1">Image 1</label>
+          <input type="file" id="image1" accept="image/*" />
+        </div>
+        <div className="image-option">
+          <label htmlFor="image2">Image 2</label>
+          <input type="file" id="image2" accept="image/*" />
+        </div>
+        <div className="image-option">
+          <label htmlFor="image3">Image 3</label>
+          <input type="file" id="image3" accept="image/*" />
+        </div>
+        <div className="image-option">
+          <label htmlFor="image4">Image 4</label>
+          <input type="file" id="image4" accept="image/*" />
+        </div>
+      </div>
     </div>
   );
 }
